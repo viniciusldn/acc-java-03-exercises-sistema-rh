a49ba41068e97cfe94204a7b8c17379995aacfdb
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-06-03 11:25:17 -0300

    :sparkles: Banco contas getter e setter

--- src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
@@ -5,14 +5,25 @@ import java.util.ArrayList;
 public class Banco {
 
   private ArrayList<PessoaCliente> pessoasClientes = new ArrayList<PessoaCliente>();
+  private ArrayList<String> contas = new ArrayList<String>();
 
   public ArrayList<PessoaCliente> getPessoasClientes() {
     return pessoasClientes;
 
   }
 
+  public ArrayList<String> getContas() {
+    return contas;
+
+  }
+
   public void setPessoasClientes(PessoaCliente pessoaCliente) {
     this.pessoasClientes.add(pessoaCliente);
+
+  }
+
+  public void setContas(String conta) {
+    this.contas.add(conta);
 
   }
 
