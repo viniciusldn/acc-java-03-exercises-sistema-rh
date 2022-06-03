ac9155fde358c2cc309f6c5fcd285f849ec02004
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-06-03 10:19:52 -0300

    :sparkles: Banco array PessoaClientes getter e setter

--- src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Banco {
 
-  ArrayList<PessoaCliente> pessoasClientes = new ArrayList<PessoaCliente>();
+  private ArrayList<PessoaCliente> pessoasClientes = new ArrayList<PessoaCliente>();
 
   public String gerarNumeroNovaConta() {
     // TODO Auto-generated method stub
@@ -14,6 +14,16 @@ public class Banco {
   public PessoaCliente adicionarPessoaCliente(PessoaCliente pessoaCliente) {
     // TODO Auto-generated method stub
     return null;
+  }
+
+  public ArrayList<PessoaCliente> getPessoasClientes() {
+    return pessoasClientes;
+
+  }
+
+  public void setPessoasClientes(PessoaCliente pessoaCliente) {
+    this.pessoasClientes.add(pessoaCliente);
+
   }
 
 }
